96923513618eba7c24f538d7903e04e11d1deae6
Joy Kimani <128522373+Joy-Kimani@users.noreply.github.com>
2023-11-24 15:47:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#pip install moviepy
 import moviepy.editor
 
+#read video file
 video = moviepy.editor.VideoFileClip('video_name.mp4')
 
+#convert video to audio
 audio = video.audio
 
+#save the audio file
 audio.write_audiofile('audio_name.mp3')
